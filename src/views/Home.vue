7eda79a4d83aaaf7c8b2b32c86f3d44f8b62c2ac
KK6666 <wwk66@outlook.com>
2019-10-08 16:22:07 +0800

    Login: yapi, localstorage-remember/autoLog

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div @click="backtoLog">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    backtoLog () {
+      this.$router.push("/")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
